b5eae6a0146ca837810b422467be08e45c6ab05e
v-inoue <tomohiro1020.i@gmail.com>
2025-07-26 17:15:35 +0900

    chore

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom' 
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider value={defaultSystem}>
-      <BrowserRouter>
+      <HashRouter> 
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </ChakraProvider>
   </React.StrictMode>
 )
