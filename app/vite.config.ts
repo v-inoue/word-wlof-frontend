d0ca4c0fc05292b4af77ac9a6f3c71ecd535077c
v-inoue <tomohiro1020.i@gmail.com>
2025-07-26 17:33:46 +0900

    chore

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: '/word-wolf-frontend/',
+   base: '/word-wlof-frontend/',
   plugins: [react()],
 })
