436a19b6797f130cc49e38940b01bead52c106bf
v-inoue <tomohiro1020.i@gmail.com>
2025-07-26 17:31:19 +0900

    chore

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/word-wolf-frontend/',
+   base: '/word-wolf-frontend/',
   plugins: [react()],
-  build: {
-    assetsDir: '', // assetsフォルダを作らず、ルート直下にファイルを配置
-  },
 })
